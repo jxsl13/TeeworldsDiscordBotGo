dd17c107cf015b795b68b8bc2db9d987f76f7d89
jxsl13 <jxsl13@gmail.com>
2020-07-29 19:30:29 +0200

    wait for signals interceptible by the app

--- main.go
+++ main.go
@@ -192,7 +192,7 @@ func main() {
 	// Wait here until CTRL-C or other term signal is received.
 	log.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
 	log.Println("Shutting down, please wait...")
 }
